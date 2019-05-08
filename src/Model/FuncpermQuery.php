7329aba89d36193330484e3c496384195b25b0f8
Paul Gomez <pauldro952@outlook.com>
2019-05-08 15:21:07 -0500

    Update FuncpermQuery.php

--- src/Model/FuncpermQuery.php
+++ src/Model/FuncpermQuery.php
@@ -12,7 +12,33 @@ use Base\FuncpermQuery as BaseFuncpermQuery;
  * long as it does not already exist in the output directory.
  *
  */
-class FuncpermQuery extends BaseFuncpermQuery
-{
+class FuncpermQuery extends BaseFuncpermQuery {
+	const HAS_PERMISSION = 'Y';
 
+	/**
+	 * Return array of function codes filtered by the permission WHERE == Y, loginid column
+	 * @param  string $userID User LoginID
+	 * @return array          Function codes
+	 */
+	public function get_users_functions($userID) {
+		$this->select('function');
+		$this->filterByPermission(self::HAS_PERMISSION);
+		$this->findByLoginid($userID);
+		$functions = $this->find()->toArray();
+		$functions[] = '';
+		return $functions;
+	}
+
+	/**
+	 * Returns if user has Y in their funcperm record
+	 * @param  string $userID   User LoginID
+	 * @param  string $function Dplus Function Code
+	 * @return bool           Does User have permission to $function?
+	 */
+	public function does_user_have_permission($userID, $function) {
+		$this->select('permission');
+		$this->filterByFunction($function);
+		$this->findByLoginid($userID);
+		return $this->findOne() == self::HAS_PERMISSION;
+	}
 }
