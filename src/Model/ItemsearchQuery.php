72bf5aa69250bcf01c9098d979cdc6dc20e30c69
Paul Gomez <pauldro952@outlook.com>
2019-06-26 13:46:39 -0500

    Filter Item Search

--- src/Model/ItemsearchQuery.php
+++ src/Model/ItemsearchQuery.php
@@ -12,7 +12,20 @@ use Base\ItemsearchQuery as BaseItemsearchQuery;
  * long as it does not already exist in the output directory.
  *
  */
-class ItemsearchQuery extends BaseItemsearchQuery
-{
+class ItemsearchQuery extends BaseItemsearchQuery {
+	/**
+     * Filter the query on the itemstatus column for Active
+     * @return $this|ItemsearchQuery The current query, for fluid interface
+     */
+	public function filterActive() {
+		return $this->filterByItemstatus(Itemsearch::ITEMSTATUS_ACTIVE);
+	}
 
+	/**
+     * Filter the query on the itemstatus column for Inactive
+     * @return $this|ItemsearchQuery The current query, for fluid interface
+     */
+	public function filterInactive() {
+		return $this->filterByItemstatus(Itemsearch::ITEMSTATUS_INACTIVE);
+	}
 }
