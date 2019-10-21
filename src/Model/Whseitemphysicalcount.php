5fb582c36ae96a4f399c4ec963579cfb8deb49b5
Paul Gomez <pauldro952@outlook.com>
2019-10-21 10:13:44 -0500

    Update Whseitemphysicalcount.php

--- src/Model/Whseitemphysicalcount.php
+++ src/Model/Whseitemphysicalcount.php
@@ -4,7 +4,6 @@ use Base\Whseitemphysicalcount as BaseWhseitemphysicalcount;
 
 use Dpluso\Model\ThrowErrorTrait;
 use Dpluso\Model\MagicMethodTraits;
-use Dpluso\Model\ItemTypeTraits;
 
 /**
  * Skeleton subclass for representing a row from the 'whseitemphysicalcount' table.
@@ -19,6 +18,34 @@ use Dpluso\Model\ItemTypeTraits;
 class Whseitemphysicalcount extends BaseWhseitemphysicalcount {
 	use ThrowErrorTrait;
 	use MagicMethodTraits;
-	use ItemTypeTraits;
 
+	/**
+	 * Returns if Item is serialized
+	 *
+	 * @param  string $itemID
+	 * @return bool           Is the Item Serialized
+	 */
+	public function is_item_serialized($itemID) {
+		return $this->type == Itemmaster::ITEMTYPE_SERIALIZED;
+	}
+
+	/**
+	 * Returns if Item is lotted
+	 *
+	 * @param  string $itemID
+	 * @return bool           Is the Item lotted
+	 */
+	public function is_item_lotted($itemID) {
+		return $this->type == Itemmaster::ITEMTYPE_LOTTED;
+	}
+
+	/**
+	 * Returns if Item is normal
+	 *
+	 * @param  string $itemID
+	 * @return bool            Is the Item normal
+	 */
+	public function is_item_normal($itemID) {
+		return $this->type == Itemmaster::ITEMTYPE_NORMAL;
+	}
 }
