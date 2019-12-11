f1799fe37fc03dba73cac21e186c714796d42e82
Paul Gomez <pauldro952@outlook.com>
2019-12-11 10:00:15 -0600

    Country

--- src/Model/Base/CountryQuery.php
+++ src/Model/Base/CountryQuery.php
@@ -82,7 +82,7 @@ abstract class CountryQuery extends ModelCriteria
      * @param     string $modelName The phpName of a model, e.g. 'Book'
      * @param     string $modelAlias The alias for the model in this query, e.g. 'b'
      */
-    public function __construct($dbName = 'default', $modelName = '\\Country', $modelAlias = null)
+    public function __construct($dbName = 'dplusodb', $modelName = '\\Country', $modelAlias = null)
     {
         parent::__construct($dbName, $modelName, $modelAlias);
     }
