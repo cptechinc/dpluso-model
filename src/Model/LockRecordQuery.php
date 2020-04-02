c5045470248a089d8e69fa0d8be55823816118a3
Paul Gomez <pauldro952@hotmail.com>
2020-04-02 10:52:33 -0500

    Add Lock Date filter

--- src/Model/LockRecordQuery.php
+++ src/Model/LockRecordQuery.php
@@ -10,4 +10,29 @@ use Dpluso\Model\QueryTraits;
  */
 class LockRecordQuery extends BaseLockRecordQuery {
 	use QueryTraits;
+
+
+	/**
+     * Filter the query on the lockdate column
+     *
+     * Example usage:
+     * <code>
+     * $query->filterByLockdate('2011-03-14'); // WHERE lockdate = '2011-03-14'
+     * $query->filterByLockdate('now'); // WHERE lockdate = '2011-03-14'
+     * $query->filterByLockdate(array('max' => 'yesterday')); // WHERE lockdate > '2011-03-13'
+     * </code>
+     *
+     * @param     mixed $lockdate The value to use as filter.
+     *              Values can be integers (unix timestamps), DateTime objects, or strings.
+     *              Empty strings are treated as NULL.
+     *              Use scalar values for equality.
+     *              Use array values for in_array() equivalent.
+     *              Use associative array('min' => $minValue, 'max' => $maxValue) for intervals.
+     * @param     string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+     *
+     * @return $this|ChildLockRecordQuery The current query, for fluid interface
+     */
+	public function filterByDate($date = null, $comparison = null) {
+		$this->filterByLockdate($date, $comparison);
+	}
 }
