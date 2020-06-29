560e87fd42ed849216333100370402446d542559
Paul Gomez <pauldro952@hotmail.com>
2020-06-29 16:22:08 -0500

    Notes cart query base

--- src/Model/Base/NotesCartQuery.php
+++ src/Model/Base/NotesCartQuery.php
@@ -87,7 +87,7 @@ abstract class NotesCartQuery extends ModelCriteria
      * @param     string $modelName The phpName of a model, e.g. 'Book'
      * @param     string $modelAlias The alias for the model in this query, e.g. 'b'
      */
-    public function __construct($dbName = 'default', $modelName = '\\NotesCart', $modelAlias = null)
+    public function __construct($dbName = 'dplusodb', $modelName = '\\NotesCart', $modelAlias = null)
     {
         parent::__construct($dbName, $modelName, $modelAlias);
     }
