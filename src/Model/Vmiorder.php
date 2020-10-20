815a8eb47a9d9f9b33dd285a4904a8f7b611dbce
Paul Gomez <pauldro952@hotmail.com>
2020-10-20 13:33:27 -0500

    Time Functions for VmiOrder

--- src/Model/Vmiorder.php
+++ src/Model/Vmiorder.php
@@ -11,4 +11,33 @@ use Dpluso\Model\MagicMethodTraits;
 class VmiOrder extends BaseVmiOrder {
 	use ThrowErrorTrait;
 	use MagicMethodTraits;
+
+	const FORMAT_DATE = 'Ymd';
+	const FORMAT_TIME = 'Hi';
+
+	/**
+	 * Return Time
+	 * @param  int $digits
+	 * @return string
+	 */
+	public function time($digits = 6) {
+		return substr($this->time, 0, $digits);
+	}
+
+	/**
+	 * Return 4 digit Time prepended by 0 if needed
+	 * @return string
+	 */
+	public function timestr() {
+		$time = $this->time(4);
+		if (intval(substr($time, 0, 1)) > 2) {
+			$minutes = substr($time, 1, 2);
+			$hours = substr($time, 0, 1);
+			$hours = "0$hours";
+		} else {
+			$minutes = substr($time, 2);
+			$hours = substr($time, 0, 2);
+		}
+		return $hours.$minutes;
+	}
 }
