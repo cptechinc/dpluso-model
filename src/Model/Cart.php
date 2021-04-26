6717f9ddc312adb7924a602f9795e35bd3d50959
Paul <pauldro952@outlook.com>
2021-04-26 12:19:22 -0500

    Update Cart.php

--- src/Model/Cart.php
+++ src/Model/Cart.php
@@ -6,17 +6,9 @@ use Dpluso\Model\ThrowErrorTrait;
 use Dpluso\Model\MagicMethodTraits;
 
 /**
- * Skeleton subclass for representing a row from the 'cart' table.
- *
- *
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
- *
+ * Class for representing a row from the 'cart' table.
  */
 class Cart extends BaseCart {
 	use ThrowErrorTrait;
 	use MagicMethodTraits;
-
 }
