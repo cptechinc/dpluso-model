a1f36c11ffd023c69fde19045375ebefbb086aeb
Paul <pauldro952@outlook.com>
2021-04-26 12:19:17 -0500

    Update BillingQuery.php

--- src/Model/BillingQuery.php
+++ src/Model/BillingQuery.php
@@ -1,6 +1,7 @@
 <?php
 
 use Base\BillingQuery as BaseBillingQuery;
+use Dpluso\Model\QueryTraits;
 
 /**
  * Skeleton subclass for performing query and update operations on the 'billing' table.
@@ -12,7 +13,17 @@ use Base\BillingQuery as BaseBillingQuery;
  * long as it does not already exist in the output directory.
  *
  */
-class BillingQuery extends BaseBillingQuery
-{
+class BillingQuery extends BaseBillingQuery {
+	use QueryTraits;
+	
+	public function selectDecryptedColumn($col, $salt) {
+		$this->withColumn("cast(aes_decrypt($col, hex('$salt')) as char charset utf8)", 'decrypt');
+		$this->select('decrypt');
+	}
 
+	public function encryptColumn($col, $salt, $sessionID) {
+		$sql = "UPDATE billing SET $col = AES_ENCRYPT($col, HEX('$salt')) WHERE sessionid = :sessID";
+		$params = array(':sessID' => $sessionID);
+		$this->execute_query($sql, $params);
+	}
 }
