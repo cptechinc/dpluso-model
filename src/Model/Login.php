542afd91d473e1c8df02cb91917d1314bad67157
Paul <pauldro952@outlook.com>
2021-08-27 15:45:56 -0500

    login

--- src/Model/Login.php
+++ src/Model/Login.php
@@ -6,14 +6,7 @@ use Dpluso\Model\ThrowErrorTrait;
 use Dpluso\Model\MagicMethodTraits;
 
 /**
- * Skeleton subclass for representing a row from the 'login' table.
- *
- *
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
- *
+ * Class for representing a row from the 'login' table.
  */
 class Login extends BaseLogin {
 	use ThrowErrorTrait;
